4f445184ed1f4df58db6d92722564b9e6703212b
Praveen Singh <praveensingh.199@gmail.com>
2025-12-03 01:04:11 +0530

    Outlet added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,20 @@
 import { BsFillPersonFill } from "react-icons/bs";
 import { FaFaceGrinHearts } from "react-icons/fa6";
 import { FaBagShopping } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <>
       <header>
         <div className="logo_container">
-          <a href="#"><img className="myntra_home" src="images/myntra_logo.webp" alt="Myntra Home" /></a>
+          <a href="#">
+            <img
+              className="myntra_home"
+              src="images/myntra_logo.webp"
+              alt="Myntra Home"
+            />
+          </a>
         </div>
         <nav className="nav_bar">
           <a href="#">Men</a>
@@ -15,17 +22,22 @@ const Header = () => {
           <a href="#">Kids</a>
           <a href="#">Home & Living</a>
           <a href="#">Beauty</a>
-          <a href="#">Studio <sup>New</sup></a>
+          <a href="#">
+            Studio <sup>New</sup>
+          </a>
         </nav>
         <div className="search_bar">
           <span className="material-symbols-outlined search_icon">search</span>
-          <input className="search_input" placeholder="Search for products, brands and more" />
+          <input
+            className="search_input"
+            placeholder="Search for products, brands and more"
+          />
         </div>
         <div className="action_bar">
-          <div className="action_container">
+          <Link to="/login" className="action_container">
             <BsFillPersonFill />
             <span className="action_name">Profile</span>
-          </div>
+          </Link>
 
           <div className="action_container">
             <FaFaceGrinHearts />
@@ -40,6 +52,6 @@ const Header = () => {
         </div>
       </header>
     </>
-  )
-}
-export default Header
+  );
+};
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./routes/App.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Bag from "./components/Bag.jsx";
+import Home from "./routes/Home.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <Home /> },
+      {
+        path: "/bag",
+        element: <Bag />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
