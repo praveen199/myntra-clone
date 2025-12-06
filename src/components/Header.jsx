9231722c133867fb7d37af6fbda12b27ceca68f8
Praveen Singh <praveensingh.199@gmail.com>
2025-12-06 10:28:03 +0530

    Home and bag page is workign correct

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,13 +8,13 @@ const Header = () => {
     <>
       <header>
         <div className="logo_container">
-          <a href="#">
+          <Link to="/">
             <img
               className="myntra_home"
               src="images/myntra_logo.webp"
               alt="Myntra Home"
             />
-          </a>
+          </Link>
         </div>
         <nav className="nav_bar">
           <a href="#">Men</a>
@@ -44,11 +44,11 @@ const Header = () => {
             <span className="action_name">Wishlist</span>
           </div>
 
-          <a className="action_container" href="pages/bag.html">
+          <Link className="action_container" to="/bag">
             <FaBagShopping />
             <span className="action_name">Bag</span>
             <span className="bag-item-count">0</span>
-          </a>
+          </Link>
         </div>
       </header>
     </>
